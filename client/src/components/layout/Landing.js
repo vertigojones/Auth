e6c08b1d1e6f6852d3f90260123b04c7694f3dc0
owenliversidge <owen.liversidge@gmail.com>
2020-11-23 14:06:30 -0500

    cleaned up react and added semantic ui

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <>
+      <h2>Landing</h2>
+    </>
+  );
+};
+
+export default Landing;
--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server now humming along on port ${PORT}`));
