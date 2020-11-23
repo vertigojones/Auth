bf1d84daf9c24e8821c9942ed59d1cfd22b3d653
owenliversidge <owen.liversidge@gmail.com>
2020-11-23 15:26:37 -0500

    finished home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
 import NavBar from "./components/layout/NavBar";
 import Landing from "./components/layout/Landing";
 
+import background from "./library/images/hd-supply-background.jpeg";
+
 const App = () => (
-  <>
+  <AppStyles style={{ backgroundImage: `url("${background}")` }}>
     <NavBar />
     <Landing />
-  </>
+  </AppStyles>
 );
 
 export default App;
+
+const AppStyles = styled.div`
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: fixed;
+  background-size: cover;
+`;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,34 +4,40 @@ import styled from "styled-components";
 
 const Landing = () => {
   return (
-    <ComponentStyles>
-      <Container className="container">
-        <HeaderContainer>
-          <Header size="huge">HD SupplyBook</Header>
-        </HeaderContainer>
-        <Message>
-          <p>
-            Connect with fellow HD Supply and White Cap employees. Please sign
-            up for an account or log in to your current profile.
-          </p>
-        </Message>
-        <ButtonContainer>
-          <Button primary>Sign Up</Button>
-          <Button secondary>Log In</Button>
-        </ButtonContainer>
-      </Container>
-    </ComponentStyles>
+    <LandingStyles>
+      <OuterContainer>
+        <Container style={{ padding: "20px" }}>
+          <HeaderContainer>
+            <Header size="huge">HD SupplyBook</Header>
+          </HeaderContainer>
+          <Message>
+            <p>
+              Connect with fellow HD Supply and White Cap employees. Please sign
+              up for an account or log in to your current
+            </p>
+          </Message>
+          <ButtonContainer>
+            <Button.Group>
+              <Button primary>Sign Up</Button>
+              <Button.Or />
+              <Button secondary>Log In</Button>
+            </Button.Group>
+          </ButtonContainer>
+        </Container>
+      </OuterContainer>
+    </LandingStyles>
   );
 };
 
 export default Landing;
 
-const ComponentStyles = styled.div`
+const LandingStyles = styled.div`
   position: relative;
+`;
 
-  .container {
-    padding-top: 25vh;
-  }
+const OuterContainer = styled.div`
+  margin-top: 33vh;
+  background-color: rgba(255, 255, 255, 0.75);
 `;
 
 const HeaderContainer = styled.div`
