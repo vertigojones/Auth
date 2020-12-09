2a02796dc2a37bc466010d6dcbd071aa1402a62c
owenliversidge <owen.liversidge@gmail.com>
2020-12-09 08:56:39 -0500

    set up for heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 // connect to MongoDB
 connectDB();
-app.get("/", (req, res) => res.send("API working!"));
+// app.get("/", (req, res) => res.send("API working!"));
 
 // init middlewear
 app.use(express.json({ extended: false }));
@@ -15,6 +16,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set the static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3001;
 
